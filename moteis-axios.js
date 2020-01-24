ed0751d794586e155ad7dd08581875bd402e1ec9
Iago Cavalcante <iagoangelimc@gmail.com>
2020-01-24 17:37:02 -0300

    chore: recuperando só os moteis de belém

--- moteis-axios.js
+++ moteis-axios.js
@@ -1,31 +1,29 @@
 const axios = require('axios')
 const fs = require('fs')
 
-const filtros = { 
-  Filtros: { 
-    MacroId: "", 
-    MicroId: "", 
-    CidadeId: 0, 
-    PrecoId: "", 
-    DescontoId: "NAOIMPORTA",
-    SuitesId: [], 
-    Paginacao: {
-      HasPaginate: true,
-      Pagina: 1,
-      Quantidade: 10
-    }, 
-    TipoFiltro: "1", 
-    UrlAmigavel: "",
-    MoteisEm: "",
-    Latitude: 0,
-    Longitude: 0, 
-    Distancia: 0 
-  } 
+const Filtros = { 
+  MacroId: "63", 
+  MicroId: "", 
+  CidadeId: 0, 
+  PrecoId: "", 
+  DescontoId: "NAOIMPORTA",
+  SuitesId: [], 
+  Paginacao: {
+    HasPaginate: true,
+    Pagina: 1,
+    Quantidade: 100
+  }, 
+  TipoFiltro: "1", 
+  UrlAmigavel: "",
+  MoteisEm: "",
+  Latitude: 0,
+  Longitude: 0, 
+  Distancia: 0 
 }
 
 axios.post('https://www.guiademoteis.com.br/Ajax/Busca/AjaxBusca/GetResultadoBuscaAvancada', 
   {
-    filtros
+    Filtros
   })
   .then((response) => {
     console.log(response.data)
@@ -36,7 +34,7 @@ axios.post('https://www.guiademoteis.com.br/Ajax/Busca/AjaxBusca/GetResultadoBus
   })
 
 const escreveJsonMoteis = (value) => {
-  fs.writeFile('moteis-axios.json', JSON.stringify(value, null, 2), function (err) {
+  fs.writeFile(`moteis-axios.json`, JSON.stringify(value, null, 2), function (err) {
     console.log('Arquivo escrito com sucesso!');
   })
 }
